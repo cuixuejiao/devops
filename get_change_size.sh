4d8578bd68bdc563c57c2246173e56b4a632ac94
cuixuejiao <cuixj@haierubic.com>
2017-02-21 15:26:52 +0800

    [Feature]get each revision info

--- get_change_size.sh
+++ get_change_size.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Perform git commit lines statistics in recently 1 month.
+# Perform git commit change lines statistics in recently 1 month.
 # Doesn't including binary files.
 
 bash_dir=$(cd $(dirname $0);pwd)
@@ -11,9 +11,12 @@ find /home/gerrit/repositories -name '*.git' -type d | grep -v '\.repo' | grep -
 
 echo "Statistics start at "`date` > ${bash_dir}/lines.txt
 while read -r project; do
-    echo "${project} " | tr -d '\n' >> ${bash_dir}/lines.txt
-    cd ${project} && git log --all --pretty=tformat: --numstat --no-merges --since="1 month" \
-        | gawk '{ add+=$1 ; subs+=$2 ; loc+=$1-$2 } END { print add,subs,loc }' \
-        >> ${bash_dir}/lines.txt
+    cd ${project}
+    # Get changed lines for each revision.
+    while read -r rev; do
+        lines=`cd ${project} && git log -1 ${rev} --pretty=tformat: --numstat \
+            | gawk '{add+=$1 ; subs+=$2 ; loc+=$1-$2} END {print add,subs,loc}'`
+        echo "${project} ${rev} ${lines}" >> ${bash_dir}/lines.txt
+    done < <(git rev-list --all --oneline --no-merges --since="1 month" | cut -d ' ' -f1)
 done < ${bash_dir}/projects.txt
 echo "Statistics end at "`date` >> ${bash_dir}/lines.txt
